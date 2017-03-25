514019897e488dd7354f3b9e9a255a5d230a8df8
AhmadWork <A7madswe@gmail.com>
2017-03-25 12:49:40 +0300

    base files

---
+++ js/resumeBuilder.js
@@ -0,0 +1,9 @@
+var name="Ahmed Alsehaim";
+
+var formatName=HTMLheaderName.replace("%data%",name);
+$("#header").append(formatName);
+
+var role="Software engineering ";
+
+var formatRole=HTMLheaderRole.replace("%data%",role);
+$("#header").append(formatRole);
